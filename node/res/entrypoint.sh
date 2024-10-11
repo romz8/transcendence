8ee8e7b5e0ecbc4cb535d1b1ce1131f5039a9adb
dtome-pe <theonewhoknew@protonmail.com>
2024-10-11 08:32:33 +0200

    add cert creation on entrypoint script

--- node/res/entrypoint.sh
+++ node/res/entrypoint.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+export $(cat ../../.env | xargs)
+
+apt-get install -y openssl && \
+    openssl genrsa -des3 -passout pass:$SSL_KEY_PASSWORD -out server.pass.key 2048 && \
+    openssl rsa -passin pass:$SSL_KEY_PASSWORD -in server.pass.key -out server.key && \
+    rm server.pass.key && \
+    openssl req -new -key server.key -out server.csr \
+        -subj "/C=ES/ST=Catalonia/L=Barcelona/O=42/OU=VimHaters/CN=transcendence.com" && \
+    openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt 
+
+mv server.crt server.key /etc/nginx/ssl/
+
+rm server.csr
 
 npm run build &
 
